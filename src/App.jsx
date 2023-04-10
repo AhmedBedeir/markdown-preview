dc09d01d9fffdff9c8bb1ab60a32e95808aa466b
AhmedBedeir <bederahmed0@gmail.com>
2023-04-10 13:53:06 +0200

    markdown

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import data from './assets/data';
+import Split from 'react-split';
+import { AiOutlineGithub } from 'react-icons/ai';
+function App() {
+  const [markdown, setMarkdown] = useState(data);
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+  return (
+    <>
+      <h1 className="title">Markdown Previewer</h1>
+      <div className="App">
+        <Split className="split" gutterSize={3} minSize={500}>
+          <div className="editor">
+            <textarea
+              value={markdown}
+              onChange={handleChange}
+              name="editor"
+              id="editor"
+            ></textarea>
+          </div>
+          <div id="preview">
+            <ReactMarkdown rehypePlugins={[rehypeRaw]} children={markdown} />
+          </div>
+        </Split>
+        <div className="footer">
+          <a href="https://githup.com/AhmedBedeir" target="_blank">
+            <AiOutlineGithub />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,34 @@
+const value = `# Welcome to my React Markdown Previewer!
+## This is a sub-heading...
+### And here's some other cool stuff:
+Heres some code, \`<div></div>\`, between 2 backticks.
+\`\`\`
+// this is multi-line code:
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+- Even if you use dashes or asterisks.
+- And last but not least, let's not forget embedded images:
+need to add image cat 
+
+
+![cat](https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60)
+
+`;
+
+export default value;
